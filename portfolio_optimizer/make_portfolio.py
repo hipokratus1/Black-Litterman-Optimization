8eac44b37a0c025b0b98aa2f5f5896772f9b2359
Grigory Burmashev <gaburmashev@edu.hse.ru>
2023-05-15 22:43:43 +0300

    Moved optimizers

---
+++ portfolio_optimizer/make_portfolio.py
@@ -0,0 +1,81 @@
+import yfinance
+from black_litterman import black_litterman, optimizers
+
+
+class DataLoader:
+    def __init__(self, tokens, market='BTC-USD', start=None, end=None):
+        market_capitalizations = {}
+        for token in tokens:
+            try:
+                t = yfinance.Ticker(token)
+                market_capitalizations[token] = t.info['marketCap']
+            except:
+                raise ValueError(f'{token} is invalid token')
+
+        self.market_capitalizations = market_capitalizations
+
+        if start or end:
+            ohlc = yfinance.download(
+                tickers=tokens,
+                start=start,
+                end=end
+            )
+        else:
+            ohlc = yfinance.download(
+                tickers=tokens,
+                period='max'
+            )
+        self.prices = ohlc['Adj Close'].dropna()
+
+        if start or end:
+            self.market_prices = yfinance.download(
+                tickers=market,
+                start=start,
+                end=end
+            )['Adj Close']
+        else:
+            self.market_prices = yfinance.download(
+                tickers=market,
+                period='max'
+            )['Adj Close']
+
+
+def optimize_portfolio(tokens, views):
+    data = DataLoader(
+        tokens=tokens,
+    )
+
+    prices = data.prices # dataframe with historical prices of cryptocurrencies
+    market_prices = data.market_prices # smth like S&P 500
+    market_capitalizations = data.market_capitalizations # {token: capitalization}
+
+    covariance_matrix = black_litterman.get_covariance_matrix(prices)
+    risk_aversion = black_litterman.get_risk_aversion(market_prices)
+
+    market_prior = black_litterman.market_implied_prior_returns(
+        capitalization=market_capitalizations,
+        risk_aversion=risk_aversion,
+        covariance_matrix=covariance_matrix,
+        risk_free_rate=0.02
+    )
+
+    bl = black_litterman.BlackLitterman(
+        covariance_matrix=covariance_matrix,
+        prior=market_prior,
+        views=views
+    )
+
+    frontier = optimizers.EfficientFrontierOptimizer(
+        expected_returns=bl.bl_returns(),
+        covariance_matrix=bl.bl_covariance(),
+        risk_free_rate=0.02
+    )
+    frontier.optimize(
+        L2_reg=True
+    )
+
+    return frontier.get_weights(
+        df=True,
+        clean=True
+    )
+
--- portfolio_optimizer/run.py
+++ portfolio_optimizer/run.py
@@ -1,79 +1,4 @@
-import yfinance
-from black_litterman import black_litterman, optimizers
-
-
-class DataLoader:
-    def __init__(self, tokens, market='BTC-USD', start=None, end=None):
-        market_capitalizations = {}
-        for token in tokens:
-            try:
-                t = yfinance.Ticker(token)
-                market_capitalizations[token] = t.info['marketCap']
-            except:
-                raise ValueError(f'{token} is invalid token')
-
-        self.market_capitalizations = market_capitalizations
-
-        if start or end:
-            ohlc = yfinance.download(
-                tickers=tokens,
-                start=start,
-                end=end
-            )
-        else:
-            ohlc = yfinance.download(
-                tickers=tokens,
-                period='max'
-            )
-        self.prices = ohlc['Adj Close'].dropna()
-
-        self.market_prices = yfinance.download(
-            tickers=market,
-            period='max'
-        )['Adj Close']
-
-
-def optimize(tokens, views):
-    data = DataLoader(
-        tokens=tokens,
-        start='2020-01-01',
-        end='2023-01-01'
-    )
-
-    prices = data.prices # dataframe with historical prices of cryptocurrencies
-    market_prices = data.market_prices # smth like S&P 500
-    market_capitalizations = data.market_capitalizations # {token: capitalization}
-
-    covariance_matrix = black_litterman.get_covariance_matrix(prices)
-    risk_aversion = black_litterman.get_risk_aversion(market_prices)
-
-    market_prior = black_litterman.market_implied_prior_returns(
-        capitalization=market_capitalizations,
-        risk_aversion=risk_aversion,
-        covariance_matrix=covariance_matrix,
-        risk_free_rate=0.02
-    )
-
-    bl = black_litterman.BlackLitterman(
-        covariance_matrix=covariance_matrix,
-        prior=market_prior,
-        views=views
-    )
-
-    frontier = optimizers.EfficientFrontierOptimizer(
-        expected_returns=bl.bl_returns(),
-        covariance_matrix=bl.bl_covariance(),
-        risk_free_rate=0.02
-    )
-    frontier.optimize(
-        L2_reg=True
-    )
-
-    return frontier.get_weights(
-        df=True,
-        clean=True
-    )
-
+from make_portfolio import optimize_portfolio
 
 if __name__ == '__main__':
     views = {
@@ -82,7 +7,7 @@ if __name__ == '__main__':
     }
     tokens = ['BTC-USD', 'ETH-USD']
 
-    weights = optimize(
+    weights = optimize_portfolio(
         tokens=tokens,
         views=views
     )
