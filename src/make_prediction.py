d95209ee851f9bb005346e5a6eb9a7e2645ed7e6
Alexander Fleyshman <fleyshman.ae@gmail.com>
2023-05-16 19:20:54 +0300

    Make_predictions updated.

--- src/make_prediction.py
+++ src/make_prediction.py
@@ -1,51 +1,41 @@
-import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
 from messari.messari import Messari
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from statsmodels.tsa.stattools import adfuller as adf
 from statsmodels.tsa.arima.model import ARIMA
 import warnings
-import numpy as np
 
-
-def make_predict(coin, start='2020-05-01', end='2023-05-01'):
+def make_predict(coin, start='2020-05-01', end='2023-05-01', period = 7):
     messari = Messari()
     assets = [coin]
     metric = 'price'
     df = messari.get_metric_timeseries(asset_slugs=assets, asset_metric=metric, start=start, end=end)
     df = df[coin]
-
-    period = 7
-    dates = df.reset_index()['timestamp'].tolist()
     close = df['close'].tolist()
     train, test = train_test_split(close, test_size=period, shuffle=False)
-    train_dates, test_dates = train_test_split(dates, test_size=period, shuffle=False)
-    train, val = train_test_split(train, test_size=period, shuffle=False)
-    train_dates, val_dates = train_test_split(train_dates, test_size=period, shuffle=False)
 
     d = 0
     if adf(train)[1] >= 0.05:
         d = 1
 
     p_values = range(0, 6)
-    q_values = range(0, 15)
-    best_rmse = np.inf
-    arima_order = (0, 0, 0)
+    q_values = range(0, 16)
+    best_RMSE = np.inf
+    best_arima_order = (0, 0, 0)
 
-    for p in p_values:
-        for q in q_values:
+    for p_ in p_values:
+        for q_ in q_values:
             warnings.filterwarnings("ignore")
-            order = (p,d,q)
-            model_ = ARIMA(train, order=order).fit()
-            y_pred_ = model_.forecast(period)
-            rmse = mean_squared_error(val, y_pred_, squared=False)
-            if rmse < best_rmse:
-                best_rmse = rmse
-                arima_order = order
-    
-    model_ = ARIMA(close, order=arima_order).fit()
-    pred = model_.forecast(period)
-    last_pred = pred[-1]
-    change_pred = (last_pred - close[-1]) / close[-1] * 100
+            order_ = (p_,d,q_)
+            model_ = ARIMA(train, order=order_).fit()
+            test_pred_ = model_.forecast(period)
+            test_RMSE_ = mean_squared_error(test, test_pred_, squared=False)
+            if test_RMSE_ < best_RMSE:
+                best_RMSE = test_RMSE_
+                best_arima_order = order_
+
+    best_model = ARIMA(close, order=best_arima_order).fit()
+    future_pred = best_model.forecast(period)
+    change_pred = (future_pred[-1] - close[-1]) / close[-1]
     return change_pred
