844ee2f1adcb6000ed217e060e8185c344e61e70
Grigory Burmashev <gaburmashev@edu.hse.ru>
2023-05-17 00:29:06 +0300

    Added no_print helper

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,14 @@
+import sys, os
+
 RISK_FREE_RATE_DEFAULT = 0.02
 STOP = 'STOP'
 EXIT = 'EXIT'
+
+def no_print(func):
+    def _wrapper(*args, **kwargs):
+        sys.stdout = open(os.devnull, 'w')
+        output = func(*args, **kwargs)
+        sys.stdout = sys.__stdout__
+        return output
+
+    return _wrapper
