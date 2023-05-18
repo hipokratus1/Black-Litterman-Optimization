5de31371366d74c675440270f9bed8aed02d36ec
Grigory Burmashev <gaburmashev@edu.hse.ru>
2023-05-18 03:51:13 +0300

    Added time decorator

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
-import sys, os
+import sys
+import os
+import time
 
 RISK_FREE_RATE_DEFAULT = 0.02
 STOP = 'STOP'
@@ -12,6 +14,20 @@ def no_print(func):
         sys.stdout = open(os.devnull, 'w')
         output = func(*args, **kwargs)
         sys.stdout = sys.__stdout__
+        return output
+
+    return _wrapper
+
+def print_time(func):
+    """
+      Decorator for calculating time of function
+    """
+    def _wrapper(*args, **kwargs):
+        start = time.time()
+        output = func(*args, **kwargs)
+        stop = time.time()
+
+        print(f'Function took: {(stop - start):0.7f} s')
         return output
 
     return _wrapper
